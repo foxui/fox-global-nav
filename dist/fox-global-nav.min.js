b5f032bcc112577be374b6bdd0d4a349a690ca51
miller <chen.minliang@gmail.com>
2014-06-09 17:01:51 +0800

    fox-global-nav

---
+++ dist/fox-global-nav.min.js
@@ -0,0 +1,9 @@
+/*!
+ * =====================================================
+ * Foxui v0.1.0 (https://github.com/foxui/fox-global-nav)
+ * Copyright 2014 fex-team
+ * Licensed under BSD (https://github.com/foxui/fox-global-nav/blob/master/LICENSE)
+ *
+ * v0.1.0 designed by @fex-team.
+ * =====================================================
+ */
